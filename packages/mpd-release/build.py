d364c89bcd6a3b8184eaeb4257ff12dfbfce708b
Denis Krjuchkov <denis@crazydev.net>
2012-10-16 00:06:24 +0600

    mpd-release: forcibly link with libz to correct detect libid3tag

--- packages/mpd-release/build.py
+++ packages/mpd-release/build.py
@@ -1,6 +1,6 @@
 fetch('http://sourceforge.net/projects/musicpd/files/mpd/0.17.2/mpd-0.17.2.tar.bz2/download')
 
-build(options='--enable-openal')
+build(options='--enable-openal', libs='-lz')
 
 collect_system_libs(libgcc=True)
 collect_binaries('mpd.exe')
