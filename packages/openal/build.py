947d042c9e7fee12f86a730643a5144f18dd57e9
Denis Krjuchkov <denis@crazydev.net>
2012-10-19 20:05:17 +0600

    openal: don't build examples

--- packages/openal/build.py
+++ packages/openal/build.py
@@ -2,7 +2,7 @@ fetch('http://kcat.strangesoft.net/openal-releases/openal-soft-1.14.tar.bz2')
 patch('CMakeLists.txt')
 patch('Alc/alcConfig.c')
 
-build_cmake(options='-DMMDEVAPI=OFF')
+build_cmake(options='-DMMDEVAPI=OFF -DEXAMPLES=OFF')
 
 collect_binaries('OpenAL32.dll')
 collect_docs('COPYING README')
