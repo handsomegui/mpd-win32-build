dae3bd3cbd8f8ef3bf1f3fef3d788fd3c0a06a20
Denis Krjuchkov <denis@crazydev.net>
2012-11-03 17:58:01 +0600

    mpd: statically link with libgcc

--- packages/mpd-release/build.py
+++ packages/mpd-release/build.py
@@ -1,8 +1,7 @@
 fetch('http://sourceforge.net/projects/musicpd/files/mpd/0.17.2/mpd-0.17.2.tar.bz2/download')
 
-build(options='--enable-openal', libs='-lz -lole32')
+build(options='--enable-openal', libs='-lz -lole32 -static-libgcc')
 
-collect_system_libs(libgcc=True)
 collect_binaries('mpd.exe')
 collect_docs('AUTHORS COPYING NEWS README')
 collect_files('doc/mpdconf.example', target_dir='conf')
--- packages/mpd-stable/build.py
+++ packages/mpd-stable/build.py
@@ -1,8 +1,7 @@
 clone('git://git.musicpd.org/master/mpd.git', 'v0.17.x')
 
-build(options='--enable-openal', libs='-lz -lole32')
+build(options='--enable-openal', libs='-lz -lole32 -static-libgcc')
 
-collect_system_libs(libgcc=True)
 collect_binaries('mpd.exe')
 collect_docs('AUTHORS COPYING NEWS README')
 collect_files('doc/mpdconf.example', target_dir='conf')
