a0c968e7eba2d35134b060a863e4b740b0d9ac53
Denis Krjuchkov <denis@crazydev.net>
2012-11-05 04:02:51 +0600

    Switch mpd-release to git mirror instead of tarball
    Last released tarball does not have all required files to build out of the box

--- packages/mpd/release-build.py
+++ packages/mpd/release-build.py
@@ -1,3 +1,4 @@
-fetch('http://sourceforge.net/projects/musicpd/files/mpd/0.17.2/mpd-0.17.2.tar.bz2/download')
+#fetch('http://sourceforge.net/projects/musicpd/files/mpd/0.17.2/mpd-0.17.2.tar.bz2/download')
+fetch('git://git.musicpd.org/master/mpd.git', 'release-0.17.2')
 
 include('common/build')
