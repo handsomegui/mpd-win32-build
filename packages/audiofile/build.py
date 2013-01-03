8d225d955a737e229a5554da469033ceae132da7
Denis Krjuchkov <denis@crazydev.net>
2013-01-03 21:36:44 +0600

    Reintroduce libaudiofile support

--- packages/audiofile/build.py
+++ packages/audiofile/build.py
@@ -1,7 +1,4 @@
 fetch('http://audiofile.68k.org/audiofile-0.3.4.tar.gz')
 
-build(shared_lib=True)
-
-collect_system_libs(libgcc=True, libstdcxx=True)
-collect_binaries('libaudiofile-*.dll')
+build(static_lib=True)
 collect_licenses('ACKNOWLEDGEMENTS AUTHORS COPYING README')
