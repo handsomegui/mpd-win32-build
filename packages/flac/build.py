2ea710c71fc399d387ab48a04aece16b47a9c014
Denis Krjuchkov <denis@crazydev.net>
2013-01-03 21:16:31 +0600

    Update flac to more recent version
    There are some changes in the build system introduced in rev 66c0a581d2.
    They prevent my scripts from working.
    Let's stick to the rev f20164e41ed until they are fixed.

--- packages/flac/build.py
+++ packages/flac/build.py
@@ -1,4 +1,4 @@
-fetch('git://git.xiph.org/flac.git', 'a2923e64c0c1')
+fetch('git://git.xiph.org/flac.git', 'f20164e41ed')
 patch('Makefile.am')
 patch('configure.ac')
 patch('src/share/utf8/utf8.c')
