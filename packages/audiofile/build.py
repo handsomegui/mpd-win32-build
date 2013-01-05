eacb12a3e610be529943bebe0195651c6dc25b71
Denis Krjuchkov <denis@crazydev.net>
2013-01-05 14:43:11 +0600

    Improve building of libaudiofile
    It seems that -lstdc++ is required for linking example programs
    when the library is statically compiled.

--- packages/audiofile/build.py
+++ packages/audiofile/build.py
@@ -1,4 +1,4 @@
 fetch('http://audiofile.68k.org/audiofile-0.3.4.tar.gz')
 
-build(static_lib=True)
+build(static_lib=True, libs='-lstdc++')
 collect_licenses('ACKNOWLEDGEMENTS AUTHORS COPYING README')
